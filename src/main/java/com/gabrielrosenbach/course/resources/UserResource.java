8406963d4e806b19459ac087f31b02979cf49b69
Gabriel Rosenbach <gabrielrosenbach@hotmail.com>
2020-03-01 21:41:49 -0300

    Service layer, component registration

--- src/main/java/com/gabrielrosenbach/course/resources/UserResource.java
+++ src/main/java/com/gabrielrosenbach/course/resources/UserResource.java
@@ -1,20 +1,37 @@
 package com.gabrielrosenbach.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabrielrosenbach.course.entities.User;
+import com.gabrielrosenbach.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
+	public ResponseEntity<List<User>> findAll() {
 		
-		User user = new User(1l, "Maria", "maria@gmail.com", "999999999", "123456");
-		return ResponseEntity.ok(user);
+		List<User> listUsers = userService.findAll();
+		
+		return ResponseEntity.ok().body(listUsers);
+	}
+	
+	@GetMapping(value = "/{code}")
+	public ResponseEntity<User> findById(@PathVariable Long code) {
+		
+		User user = userService.findById(code);
+		
+		return ResponseEntity.ok().body(user);
 	}
 }
---
+++ src/main/java/com/gabrielrosenbach/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.gabrielrosenbach.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielrosenbach.course.entities.User;
+import com.gabrielrosenbach.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findAll() {
+
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long code) {
+		
+		Optional<User> user = userRepository.findById(code);
+		
+		return user.get();
+	}
+ }
